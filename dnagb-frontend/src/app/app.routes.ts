fc2bd0c2e294aad2b95654e8b8f8af9e7beb2e00
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-06-10 14:55:10 +0200

    add routes, components & basic layout

--- dnagb-frontend/src/app/app.routes.ts
+++ dnagb-frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ContactComponent } from './contact/contact.component';
+import { UpToDateComponent } from './up-to-date/up-to-date.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Startseite - DNagB',
+  },
+  {
+    path: 'up-to-date',
+    component: UpToDateComponent,
+    title: 'Aktuelles - DNagB',
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent,
+    title: 'Impressum - DNagB',
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
+    title: 'Datenschutz - DNagB',
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Kontakt - DNagB',
+  },
+];
