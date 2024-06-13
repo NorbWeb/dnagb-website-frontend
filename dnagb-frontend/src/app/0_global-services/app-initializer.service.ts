4c1af6d652cb82693d334a6a4c1e9f59402b805f
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-06-13 14:59:50 +0200

    add services and color logic

--- dnagb-frontend/src/app/0_global-services/app-initializer.service.ts
+++ dnagb-frontend/src/app/0_global-services/app-initializer.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environment/env';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppInitializerService {
+  utils = inject(UtilsService);
   init(): Promise<void> {
     return new Promise(async (resolve, reject) => {
       const settings = await this.getAppSettings().catch(reject);
       console.log('🐦‍⬛: AppInitializerService -> init', settings);
+      this.setColors(
+        settings.data.primary,
+        settings.data.primary_text,
+        settings.data.secondary,
+        settings.data.secondary_text
+      );
       resolve();
     });
   }
@@ -16,6 +24,37 @@ export class AppInitializerService {
   private getAppSettings() {
     return fetch(`${environment.cmsUrl}/items/settings`).then((res) =>
       res.json()
+    );
+  }
+
+  private setColors(
+    primary: string,
+    primary_text: string,
+    secondary: string,
+    secondary_text: string
+  ) {
+    document.documentElement.style.setProperty(
+      '--primary-lighten',
+      this.utils.calculateColor(primary, 'lighten')
+    );
+    document.documentElement.style.setProperty('--primary', primary);
+    document.documentElement.style.setProperty(
+      '--primary-darken',
+      this.utils.calculateColor(primary, 'darken')
+    );
+    document.documentElement.style.setProperty(
+      '--secondary-lighten',
+      this.utils.calculateColor(secondary, 'lighten')
+    );
+    document.documentElement.style.setProperty('--secondary', secondary);
+    document.documentElement.style.setProperty(
+      '--secondary-darken',
+      this.utils.calculateColor(secondary, 'darken')
+    );
+    document.documentElement.style.setProperty('--primary-text', primary_text);
+    document.documentElement.style.setProperty(
+      '--secondary-text',
+      secondary_text
     );
   }
 }
