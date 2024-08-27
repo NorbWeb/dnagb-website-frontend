e9c11c31fe7b321d1643340070c1ee4ba3e26007
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-08-27 22:12:26 +0200

    add prev and next month logic

--- dnagb-frontend/src/app/home/calendar/calendar.component.ts
+++ dnagb-frontend/src/app/home/calendar/calendar.component.ts
@@ -11,7 +11,9 @@ export class CalendarComponent implements OnInit {
   date = new Date();
   currentYear = this.date.getFullYear();
   currentMonth = this.date.getMonth() + 1;
-  daysInCurrentMonth: number[] = [];
+  displayedDays: number[] = [];
+  weekday = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  monthCounter: number = 0;
 
   constructor() {}
 
@@ -20,11 +22,47 @@ export class CalendarComponent implements OnInit {
   }
 
   initCalendar() {
-    let days = this.getDaysInMonth(this.currentYear, this.currentMonth);
+    let days = this.getDaysInMonth(this.currentYear, this.currentMonth); // Number of days in current month
+    let firstDay = new Date(`${this.currentYear}-${this.currentMonth}-01`); // To get start weekday
+    let lastDay = new Date(this.currentYear, this.currentMonth, 0); // To get end weekday
+    let lastDayMonthBefore = this.getDaysInMonth(
+      this.currentYear,
+      this.currentMonth - 1
+    ); // Last day of month before current month
+    let startDayMonthBefore = lastDayMonthBefore - 6 + firstDay.getDay(); // Start day in month before. Is needed to get all days that fills gaps if current month did not start on monday
+    let monthBefor = []; // Holds all days for weekday gap before current month
+    let month = []; // Holds all days of current month
+    let monthAfter = []; // Holds all days for weekday gap after current month
+
+    if (firstDay.getDay() !== 0) {
+      for (let b = startDayMonthBefore; b <= lastDayMonthBefore; b++) {
+        monthBefor.push(b);
+      }
+    }
 
     for (let i = 1; i <= days; i++) {
-      this.daysInCurrentMonth?.push(i);
+      month.push(i);
     }
+
+    if (lastDay.getDay() !== 0) {
+      for (let a = 1; a < 7 - lastDay.getDay(); a++) {
+        monthAfter.push(a);
+      }
+    }
+
+    this.displayedDays = [...monthBefor, ...month, ...monthAfter];
+  }
+
+  nextMonth() {
+    ++this.monthCounter;
+    this.currentMonth = this.date.getMonth() + this.monthCounter;
+    this.initCalendar();
+  }
+
+  prevMonth() {
+    --this.monthCounter;
+    this.currentMonth = this.date.getMonth() - this.monthCounter;
+    this.initCalendar();
   }
 
   ngOnInit(): void {
