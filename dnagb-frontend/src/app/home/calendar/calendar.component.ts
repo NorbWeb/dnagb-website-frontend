5083b94669872ebe2ee70b856db885f33f45c7b4
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-08-28 13:51:58 +0200

    edit calendar logic

--- dnagb-frontend/src/app/home/calendar/calendar.component.ts
+++ dnagb-frontend/src/app/home/calendar/calendar.component.ts
@@ -8,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './calendar.component.css',
 })
 export class CalendarComponent implements OnInit {
+  weekday = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  month = [
+    'Januar',
+    'Februar',
+    'März',
+    'April',
+    'Mai',
+    'Juni',
+    'Juli',
+    'August',
+    'September',
+    'Oktober',
+    'November',
+    'Dezember',
+  ];
   date = new Date();
   currentYear = this.date.getFullYear();
-  currentMonth = this.date.getMonth() + 1;
-  displayedDays: number[] = [];
-  weekday = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  currentMonth = this.date.getMonth();
+  displayedDays: any[] = [];
   monthCounter: number = 0;
 
   constructor() {}
@@ -22,46 +36,61 @@ export class CalendarComponent implements OnInit {
   }
 
   initCalendar() {
-    let days = this.getDaysInMonth(this.currentYear, this.currentMonth); // Number of days in current month
-    let firstDay = new Date(`${this.currentYear}-${this.currentMonth}-01`); // To get start weekday
-    let lastDay = new Date(this.currentYear, this.currentMonth, 0); // To get end weekday
+    let days = this.getDaysInMonth(this.currentYear, this.currentMonth + 1); // Number of days in current month
+    let firstDay = new Date(
+      `${this.currentYear}-${this.currentMonth + 1}-01`
+    ).getDay(); // To get start weekday
+
     let lastDayMonthBefore = this.getDaysInMonth(
       this.currentYear,
-      this.currentMonth - 1
+      this.currentMonth
     ); // Last day of month before current month
-    let startDayMonthBefore = lastDayMonthBefore - 6 + firstDay.getDay(); // Start day in month before. Is needed to get all days that fills gaps if current month did not start on monday
     let monthBefor = []; // Holds all days for weekday gap before current month
     let month = []; // Holds all days of current month
     let monthAfter = []; // Holds all days for weekday gap after current month
 
-    if (firstDay.getDay() !== 0) {
-      for (let b = startDayMonthBefore; b <= lastDayMonthBefore; b++) {
-        monthBefor.push(b);
-      }
+    let dayBeforeCount = lastDayMonthBefore - firstDay + 2; // Helps calculate how many days you need before actual month
+
+    if (firstDay === 0) {
+      dayBeforeCount = lastDayMonthBefore - 5;
+    }
+
+    for (let b = lastDayMonthBefore; b >= dayBeforeCount; b--) {
+      monthBefor.unshift({ day: b, inMonth: false });
     }
 
     for (let i = 1; i <= days; i++) {
-      month.push(i);
+      month.push({ day: i, inMonth: true });
     }
 
-    if (lastDay.getDay() !== 0) {
-      for (let a = 1; a < 7 - lastDay.getDay(); a++) {
-        monthAfter.push(a);
-      }
+    let countDays = (monthBefor.length + month.length) / 7; // How many days are displayed by now and divided to seven
+    let extraDays = Math.ceil(countDays); // Round up to max row in calendar
+    let diff = extraDays * 7 - countDays * 7; // Calculate extra days, that will be displayed after current month to fill gaps
+
+    for (let a = 1; a <= diff; a++) {
+      monthAfter.push({ day: a, inMonth: false });
     }
 
     this.displayedDays = [...monthBefor, ...month, ...monthAfter];
   }
 
   nextMonth() {
-    ++this.monthCounter;
-    this.currentMonth = this.date.getMonth() + this.monthCounter;
+    if (this.currentMonth === 11) {
+      this.currentMonth = 0;
+      ++this.currentYear;
+    } else {
+      ++this.currentMonth;
+    }
     this.initCalendar();
   }
 
   prevMonth() {
-    --this.monthCounter;
-    this.currentMonth = this.date.getMonth() - this.monthCounter;
+    if (this.currentMonth === 0) {
+      this.currentMonth = 11;
+      --this.currentYear;
+    } else {
+      --this.currentMonth;
+    }
     this.initCalendar();
   }
 
