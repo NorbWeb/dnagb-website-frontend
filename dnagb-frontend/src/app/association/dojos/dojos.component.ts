e664a7e6e2220a03873eb25beaa8c8d8da325c50
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-08-29 22:06:23 +0200

    edit dojo-comonent

--- dnagb-frontend/src/app/association/dojos/dojos.component.ts
+++ dnagb-frontend/src/app/association/dojos/dojos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Map, NavigationControl } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   selector: 'app-dojos',
@@ -16,11 +17,31 @@ export class DojosComponent implements OnInit, OnDestroy {
       container: 'map',
       style:
         'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json',
-      center: [10.447683, 51.163361],
-      zoom: 5.35,
+      center: [10.415, 51.356],
+      zoom: 5.3,
     });
 
-    this.map.addControl(new NavigationControl({}), 'top-right');
+    this.map.addControl(new NavigationControl({}), 'top-left');
+
+    this.map.on('move', () => {
+      console.log(this.map?.getZoom());
+    });
+
+    // this.map.on('load', () => {
+    //   this.map?.addSource('background-source', {
+    //     type: 'raster',
+    //     tiles: [
+    //       'https://sgx.geodatenzentrum.de/wms_topplus_open?bbox=939258.2035682462,7200979.560689885,1252344.271424327,7514065.628545966&service=WMS&version=1.1.0&request=GetMap&layers=web_light_grau&styles=&srs=EPSG:3857&width=256&height=256&format=image/png&transparent=true',
+    //     ],
+    //     tileSize: 256,
+    //   });
+    //   this.map?.addLayer({
+    //     id: 'background-layer',
+    //     type: 'raster',
+    //     source: 'background-source',
+    //     paint: {},
+    //   });
+    // });
   }
 
   ngOnInit(): void {
