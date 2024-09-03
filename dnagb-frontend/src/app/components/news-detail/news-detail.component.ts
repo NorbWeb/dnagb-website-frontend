996fcb2a3d0bdf5b57778812cb2e2b9628ee30a1
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-09-03 15:04:29 +0200

    edit news-details

--- dnagb-frontend/src/app/components/news-detail/news-detail.component.ts
+++ dnagb-frontend/src/app/components/news-detail/news-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StateService } from '../../0_global-services/state.service';
 import { NewsItem } from '../../1_types-and-interfaces/NewsItem';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-news-detail',
@@ -12,12 +13,29 @@ import { NewsItem } from '../../1_types-and-interfaces/NewsItem';
 })
 export class NewsDetailComponent implements OnInit {
   data!: NewsItem;
+  options: any = {
+    weekday: 'short',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    // hour: '2-digit',
+    // minute: '2-digit',
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private state: StateService,
+    private location: Location
+  ) {}
 
-  constructor(private route: ActivatedRoute, private state: StateService) {}
+  goBackToPrevPage(): void {
+    this.location.back();
+  }
 
   ngOnInit(): void {
     const { id } = this.route.snapshot.params;
-    this.data = this.state.getConf().news.find((f: { id: any }) => f.id == id);
+    this.data = this.state
+      .getConf()
+      .news.find((f: { id: any }) => Number(f.id) === Number(id));
     console.log(this.data, id);
   }
 }
