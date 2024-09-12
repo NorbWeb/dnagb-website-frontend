74f8c9c05f9d1547e7e19afc518d44f5b2736af7
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-09-12 14:40:57 +0200

    add dojo marker on map

--- dnagb-frontend/src/app/components/dojos/dojos.component.ts
+++ dnagb-frontend/src/app/components/dojos/dojos.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Map, NavigationControl } from 'maplibre-gl';
+import { Map, Marker, NavigationControl } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import { StateService } from '../../0_global-services/state.service';
+import { environment } from '../../../environment/env';
+
+interface DojoInfo {
+  name: string;
+  city: string;
+  link: string;
+  description: string;
+  logo: string;
+}
 
 @Component({
   selector: 'app-dojos',
@@ -11,6 +21,21 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 })
 export class DojosComponent implements OnInit, OnDestroy {
   map: Map | undefined;
+  url = environment.cmsUrl;
+  dojoInfo: DojoInfo = {
+    name: '',
+    city: '',
+    link: '',
+    description: '',
+    logo: '',
+  };
+  showInfo: boolean = false;
+
+  constructor(private state: StateService) {}
+
+  closeInfo() {
+    this.showInfo = false;
+  }
 
   initMap() {
     this.map = new Map({
@@ -19,6 +44,8 @@ export class DojosComponent implements OnInit, OnDestroy {
         'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json',
       center: [10.415, 51.356],
       zoom: 5.3,
+      maxZoom: 10,
+      minZoom: 4.5,
     });
 
     this.map.addControl(new NavigationControl({}), 'top-left');
@@ -42,6 +69,51 @@ export class DojosComponent implements OnInit, OnDestroy {
     //     paint: {},
     //   });
     // });
+
+    const dojos = this.state.getConf().association.dojos;
+    console.log('🐦‍⬛: DojosComponent -> initMap -> dojos', dojos);
+
+    for (const dojo of dojos) {
+      const el = document.createElement('div');
+      el.className = 'dojo-marker';
+
+      el.style.backgroundImage = `url(${this.url}/assets/${dojo.logo})`;
+      el.style.width = `40px`;
+      el.style.height = `40px`;
+      el.style.borderRadius = '50%';
+      el.style.border = '0.1rem solid var(--grey-500)';
+      el.style.filter = 'drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25))';
+      el.style.backgroundColor = 'var(--white)';
+      el.style.backgroundSize = 'cover';
+      el.style.backgroundPosition = 'center';
+      el.style.cursor = 'pointer';
+
+      el.addEventListener('click', () => {
+        this.showInfo = true;
+        this.dojoInfo = {
+          name: '',
+          city: '',
+          link: '',
+          description: '',
+          logo: '',
+        };
+
+        if (dojo.status === 'published') {
+          this.dojoInfo.name = dojo.name;
+          this.dojoInfo.city = dojo.city;
+          this.dojoInfo.link = dojo.link;
+          this.dojoInfo.description = dojo.description;
+          this.dojoInfo.logo = dojo.logo;
+        }
+        console.log(
+          '🐦‍⬛: DojosComponent -> initMap -> this.dojoInfo',
+          this.dojoInfo
+        );
+      });
+      new Marker({ element: el })
+        .setLngLat(dojo.coordinates.coordinates)
+        .addTo(this.map);
+    }
   }
 
   ngOnInit(): void {
