e5399d836fe23128026ebcc8d90d2f48247901c5
Norbert Madauß <norbert.madauss@delphi-imm.de>
2024-09-24 08:18:52 +0200

    edit downloads

--- dnagb-frontend/src/app/components/download/download.component.ts
+++ dnagb-frontend/src/app/components/download/download.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StateService } from '../../0_global-services/state.service';
+import { environment } from '../../../environment/env';
 
 @Component({
   selector: 'app-download',
@@ -11,15 +12,32 @@ import { StateService } from '../../0_global-services/state.service';
 export class DownloadComponent {
   files!: any;
   headers: string[] = [];
+  url = environment.cmsUrl;
+
   constructor(private state: StateService) {}
+
+  directDownload(e: Event, url: string) {
+    e.stopPropagation();
+    window.location.assign(url);
+  }
 
   ngOnInit(): void {
     if (this.state.getConf().downloads.status === 'published')
       this.files = this.state.getConf().downloads.files;
+    this.files.sort((a: any, b: any) => {
+      if (a.data.title.toLowerCase() > b.data.title.toLowerCase()) return 1;
+      if (a.data.title.toLowerCase() < b.data.title.toLowerCase()) return -1;
+      return 0;
+    });
     for (const file of this.files) {
-      this.headers.push(file.data.tags);
+      file.data.tags.map((item: string) =>
+        this.headers.includes(item) ? null : this.headers.push(item)
+      );
     }
-
-    console.log(this.headers);
+    this.headers.sort((a, b) => {
+      if (a.toLowerCase() > b.toLowerCase()) return 1;
+      if (a.toLowerCase() < b.toLowerCase()) return -1;
+      return 0;
+    });
   }
 }
