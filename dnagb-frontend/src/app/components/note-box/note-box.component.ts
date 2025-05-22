51040f92592c4bcc43ce4b58a60d263a26f64d1a
Norbert Madauß <norbert.madauss@delphi-imm.de>
2025-05-22 07:00:30 +0200

    edit note box

--- dnagb-frontend/src/app/components/note-box/note-box.component.ts
+++ dnagb-frontend/src/app/components/note-box/note-box.component.ts
@@ -4,6 +4,7 @@ import { EventItem } from '../../1_types-and-interfaces/NewsItem';
 import { Subject, takeUntil } from 'rxjs';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environment/env';
 
 @Component({
   selector: 'app-note-box',
@@ -15,14 +16,16 @@ export class NoteBoxComponent implements OnInit, OnDestroy {
   unsubscribeAll = new Subject();
   data!: EventItem;
   open: boolean = false;
+  url = environment.cmsUrl;
 
   constructor(private state: StateService, private router: Router) {}
 
   navigateToEvent() {
-    this.router.navigateByUrl(`/news-details/${this.data.id}`);
+    this.router.navigateByUrl(`/event-details/${this.data.id}`);
   }
 
   closeEventBox() {
+    console.log('closeEventBox');
     this.state.updateNoteBox({ ...this.state.getNoteBox, open: false });
   }
 
