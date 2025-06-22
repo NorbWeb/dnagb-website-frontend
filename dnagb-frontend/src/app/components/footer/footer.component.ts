e0c8569bb0a906f6a4efe17499caf240268a9447
Norbert Madauß <norbert.madauss@delphi-imm.de>
2025-06-22 13:30:42 +0200

    edit footer

--- dnagb-frontend/src/app/components/footer/footer.component.ts
+++ dnagb-frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { footerNavData } from './footer.nav.data';
 
 @Component({
-    selector: 'app-footer',
-    imports: [RouterLink, RouterLinkActive],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.css'
+  selector: 'app-footer',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  footerNavData = signal<any>(footerNavData);
+}
